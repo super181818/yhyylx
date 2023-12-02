a4922babba751e0eede4cbbe9113eace684f9113
super181818 <131360198+super181818@users.noreply.github.com>
2023-12-02 22:32:15 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*-------config start-------*/
+var _wip = 'yylx.panguh5.com';  
+var _port = '1730';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10000' ;
+BaseUrl = 'http://yyds.panguh5.com:1998/' + _port ;
+_wport = '1997' ;
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/dyoss.js?v=' + _tt + '"> <\/script>');*/
